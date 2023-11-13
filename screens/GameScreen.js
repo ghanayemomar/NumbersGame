953c3bb79d7440b194e82f6b02e66df3132dcc1c
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-11-13 21:25:55 +0200

    Respecting Device Screen Restrictions with the SafeAreaView

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,22 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function GameScreen() {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      <View>
+        <Text>Log Rounds</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
